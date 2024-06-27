4a69c85e9c5d02c6880820d99fa10b0f3c373daa
Giulio <giulio.appetito.ga@gmail.com>
2024-06-28 01:02:13 +0200

    fixing file dir

--- producer/producer.py
+++ producer/producer.py
@@ -3,18 +3,20 @@ import time
 import json
 from kafka import KafkaProducer
 import os
+from datetime import datetime
 
 # Configuration
 KAFKA_BROKER = os.getenv('BOOTSTRAP_SERVERS', 'localhost:9092')
 TOPIC_NAME = os.getenv('TOPIC_NAME', 'hdd_events')
 CSV_FILE_PATH = '/home/producer/data/raw_data_medium-utv_sorted.csv'
-DELAY = 1  # Delay in seconds between sending each row
+SCALE_FACTOR = 3600  # 1 hour becomes 1 second
 
 # Create a Kafka producer
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BROKER,
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
+
 
 def preprocess_data(row):
     preprocessed_row = {
@@ -23,24 +25,38 @@ def preprocess_data(row):
         "model": row[2],
         "failure": row[3],
         "vault_id": int(row[4]),
-        "power_on_hours": int(row[9]),
-        "temperature_celsius": int(row[32]) if row[32] else None
+        "power_on_hours": int(row[12]),
+        "temperature_celsius": int(row[25]) if row[25] else None
     }
     return preprocessed_row
+
 
 def send_to_kafka(row):
     producer.send(TOPIC_NAME, row)
     producer.flush()
     print(f"Successfully sent data to Kafka: {row}")
 
+
 def main():
     with open(CSV_FILE_PATH, mode='r') as file:
         csv_reader = csv.reader(file)
         header = next(csv_reader)  # Skip the header row
+
+        previous_timestamp = None
+
         for row in csv_reader:
+            current_timestamp = datetime.strptime(row[0], "%Y-%m-%dT%H:%M:%S.%f")
+
+            if previous_timestamp:
+                interval = (current_timestamp - previous_timestamp).total_seconds()
+                scaled_interval = interval / SCALE_FACTOR
+                time.sleep(scaled_interval)
+
             preprocessed_row = preprocess_data(row)
             send_to_kafka(preprocessed_row)
-            time.sleep(DELAY)
+
+            previous_timestamp = current_timestamp
+
 
 if __name__ == '__main__':
     main()
