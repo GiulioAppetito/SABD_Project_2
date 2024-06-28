2a9d3f866c5d945f837369c543c7a1e7385d99fd
Giulio <giulio.appetito.ga@gmail.com>
2024-06-28 15:19:36 +0200

    welford algoritm Q1

--- flink/src/flink_job_query1.py
+++ flink/src/flink_job_query1.py
@@ -1,31 +1,23 @@
-import json
-from pyflink.common.serialization import SimpleStringSchema
-from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.datastream.connectors.kafka import FlinkKafkaConsumer
+# WELFORD ALGORITHM
+# For a new value new_value, compute the new count, new mean, the new M2.
+# mean accumulates the mean of the entire dataset
+# M2 aggregates the squared distance from the mean
+# count aggregates the number of samples seen so far
+def update(existing_aggregate, new_value):
+    (count, mean, M2) = existing_aggregate
+    count += 1
+    delta = new_value - mean
+    mean += delta / count
+    delta2 = new_value - mean
+    M2 += delta * delta2
+    return count, mean, M2
 
 
-def main():
-    env = StreamExecutionEnvironment.get_execution_environment()
-    env.set_parallelism(1)
-
-    properties = {
-        'bootstrap.servers': 'localhost:9092',
-        'group.id': 'flink_consumer'
-    }
-
-    kafka_consumer = FlinkKafkaConsumer(
-        topics='hdd_events',
-        deserialization_schema=SimpleStringSchema(),
-        properties=properties
-    )
-
-    stream = env.add_source(kafka_consumer)
-
-    # Print each message to the console log
-    stream.map(lambda x: json.loads(x)).print()
-
-    env.execute('Flink Streaming Job')
-
-
-if __name__ == '__main__':
-    main()
+# Retrieve the mean, variance and sample variance from an aggregate
+def finalize(existing_aggregate):
+    (count, mean, M2) = existing_aggregate
+    if count < 2:
+        return float("nan")
+    else:
+        (mean, variance, sample_variance) = (mean, M2 / count, M2 / (count - 1))
+        return mean, variance, sample_variance
