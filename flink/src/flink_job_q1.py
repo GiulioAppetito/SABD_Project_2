f1aca23eca8e24e1894c423eac344700f760c7ee
Giulio <giulio.appetito.ga@gmail.com>
2024-07-03 09:47:09 +0200

    Working Flink job: reads and writes on Kafka, filter+map

--- flink/src/flink_job_q1.py
+++ flink/src/flink_job_q1.py
@@ -2,6 +2,8 @@ from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.connectors.kafka import FlinkKafkaConsumer, FlinkKafkaProducer
 from pyflink.datastream.formats.json import JsonRowDeserializationSchema, JsonRowSerializationSchema
 from pyflink.common.typeinfo import Types
+from pyflink.common import Row
+
 
 def main():
     env = StreamExecutionEnvironment.get_execution_environment()
@@ -35,8 +37,9 @@ def main():
     kafka_sink_topic = 'filtered_hdd_events'
 
     serialization_schema = JsonRowSerializationSchema.builder().with_type_info(
-        Types.ROW_NAMED(["date", "serial_number", "model", "failure", "vault_id", "s9_power_on_hours", "s194_temperature_celsius"],
-                        [Types.STRING(), Types.STRING(), Types.STRING(), Types.BOOLEAN(), Types.INT(), Types.FLOAT(), Types.FLOAT()])
+        Types.ROW_NAMED(["vault_id", "s194_temperature_celsius"],
+                        [Types.INT(), Types.FLOAT()]
+                        )
     ).build()
 
     kafka_producer = FlinkKafkaProducer(
@@ -49,7 +52,12 @@ def main():
     kafka_stream = env.add_source(kafka_consumer)
 
     # Filtrare i dati
-    filtered_stream = kafka_stream.filter(lambda x: 1000 <= x['vault_id'] <= 1020)
+    filtered_stream = (kafka_stream
+                       .filter(lambda x: 1000 <= x.vault_id <= 1020)
+                       .map(lambda x: Row(vault_id=x.vault_id, s194_temperature_celsius=x.s194_temperature_celsius),
+                            Types.ROW_NAMED(["vault_id", "s194_temperature_celsius"], [Types.INT(), Types.FLOAT()]))
+                       )
+
     filtered_stream.add_sink(kafka_producer)
 
     env.execute("Flink Kafka Filter and Forward Job")
