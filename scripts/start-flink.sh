27b6315b49f3ea49f2458581b240d651a959df6b
GiulioAppetito <72864708+GiulioAppetito@users.noreply.github.com>
2024-07-09 16:40:37 +0200

    Update start-flink.sh

--- scripts/start-flink.sh
+++ scripts/start-flink.sh
@@ -3,7 +3,7 @@
 echo "Starting Flink job..."
 
 usage() {
-  echo "Usage: $0 [--job1|--job2|--both] [--1d|--3d|--all|--all_three]"
+  echo "Usage: $0 [--job1|--job2|--both] [--1d|--3d|--all|--all_three] [true|false]"
   exit 1
 }
 
@@ -15,9 +15,9 @@ run_flink_job() {
   job_name=$1
   script_path=$2
   window_type=$3
-
+  flag=$4
   echo "Executing Flink job: $job_name with window type: $window_type"
-  if docker exec jobmanager /bin/bash -c "/opt/flink/bin/flink run -py $script_path $window_type"; then
+  if docker exec -d jobmanager /bin/bash -c "/opt/flink/bin/flink run -py $script_path $window_type $flag"; then
     echo "Flink job for $job_name started successfully!"
   else
     echo "Failed to start Flink job for $job_name..."
@@ -37,70 +37,40 @@ run_consumer() {
 
 job_type=$1
 window_type=$2
+window=$(echo "$2" | sed 's/--//g')
+flag=$3
+
 
 case $job_type in
-  --job1)
-    run_flink_job "query1" "/opt/flink/jobs/src/flink_job_q1.py" "$window_type"
-    case $window_type in
-      --1d)
-        run_consumer "query1_1d_results"
-        ;;
-      --3d)
-        run_consumer "query1_3d_results"
-        ;;
-      --all)
-        run_consumer "query1_all_results"
-        ;;
-      --all_three)
-        run_consumer "query1_1d_results" "query1_3d_results" "query1_all_results"
-        ;;
-      *)
-        usage
-        ;;
-    esac
+  job1)
+    run_flink_job "query1" "/opt/flink/jobs/src/flink_job_q1.py" "$window" "$flag"
     ;;
-  --job2)
-    run_flink_job "query2" "/opt/flink/jobs/src/flink_job_q2.py" "$window_type"
-    case $window_type in
-      --1d)
-        run_consumer "query2_1d_results"
-        ;;
-      --3d)
-        run_consumer "query2_3d_results"
-        ;;
-      --all)
-        run_consumer "query2_all_results"
-        ;;
-      --all_three)
-        run_consumer "query2_1d_results" "query2_3d_results" "query2_all_results"
-        ;;
-      *)
-        usage
-        ;;
-    esac
+  job2)
+    run_flink_job "query2" "/opt/flink/jobs/src/flink_job_q2.py" "$window" $flag
     ;;
-  --both)
-    run_flink_job "query1" "/opt/flink/jobs/src/flink_job_q1.py" "$window_type"
-    run_flink_job "query2" "/opt/flink/jobs/src/flink_job_q2.py" "$window_type"
-    case $window_type in
-      --1d)
-        run_consumer "query1_1d_results" "query2_1d_results"
-        ;;
-      --3d)
-        run_consumer "query1_3d_results" "query2_3d_results"
-        ;;
-      --all)
-        run_consumer "query1_all_results" "query2_all_results"
-        ;;
-      --all_three)
-        run_consumer "query1_1d_results" "query1_3d_results" "query1_all_results" "query2_1d_results" "query2_3d_results" "query2_all_results"
-        ;;
-      *)
-        usage
-        ;;
-    esac
+  both)
+    run_flink_job "query1" "/opt/flink/jobs/src/flink_job_q1.py" "$window" $flag
+    run_flink_job "query2" "/opt/flink/jobs/src/flink_job_q2.py" "$window" $flag
     ;;
   *)
     usage
+    exit 1
     ;;
+esac
+case $window_type in
+      1d)
+        run_consumer "query1_1d_results" "query2_1d_results"
+        ;;
+      3d)
+        run_consumer "query1_3d_results" "query2_3d_results"
+        ;;
+      all)
+        run_consumer "query1_all_results" "query2_all_results"
+        ;;
+      all_three)
+        run_consumer "query1_1d_results" "query1_3d_results" "query1_all_results" "query2_1d_results" "query2_3d_results" "query2_all_results"
+        ;;
+      *)
+        echo "Consumer did not start up correctly"
+        ;;
 esac
